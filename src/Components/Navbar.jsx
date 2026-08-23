da42e52475cdbe460e248909c3876d6a60a9f4fd
Abdul Mannan <abdulmannan010spn@gmail.com>
2026-08-24 00:14:02 +0530

    Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,33 @@
+import { navIcons, navLinks } from "../Constants"
+
 const Navbar = () => {
   return (
-    <div>
-        
-    </div>
+    <nav>
+
+        <div>
+            <img src="/images/logo.svg" alt="logo" />
+            <p className="font-medium">
+                Abdul Mannan
+            </p>
+
+            <ul>
+                {navLinks.map(({id,name})=>(
+                    <li key={id}>
+                        <p>{name}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+
+        <ul>
+            {navIcons.map(({id,img})=>(
+                <li key={id}>
+                    <img src={img} className="icon-hover" alt={`icon-${id}`}/>
+                </li>
+            ))}
+        </ul>
+
+    </nav>
   )
 }
 export default Navbar
