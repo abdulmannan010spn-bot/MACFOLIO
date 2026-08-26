6e0f45c2b0ed10051cf5f90593d5496cb6755dbd
Abdul Mannan <abdulmannan010spn@gmail.com>
2026-08-26 23:26:10 +0530

    Terminal Working

--- src/Hoc/WindowWrapper.jsx
+++ src/Hoc/WindowWrapper.jsx
@@ -1,12 +1,43 @@
-import { useRef } from "react";
+import { useLayoutEffect, useRef } from "react";
 import useWindowStore from "../Stores/Window";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const WindowWrapper = (Component, windowKey) => {
   const Wrapped = (props) => {
+    // eslint-disable-next-line no-unused-vars
     const { focusWindow, windows } = useWindowStore();
     const { isOpen, zIndex } = windows[windowKey];
     const ref = useRef(null);
 
+    useGSAP(() => {
+      const el = ref.current;
+      if (!el || !isOpen) return;
+
+      el.style.display = "block";
+
+      gsap.fromTo(
+        el,
+        {
+          scale: 0.8,
+          opacity: 0,
+          y: 40,
+        },
+        {
+          scale: 1,
+          y: 0,
+          opacity:1,
+          duration: 0.3,
+          ease: "power.inOut",
+        },
+      );
+    }, [isOpen]);
+
+    useLayoutEffect(() => {
+      const el = ref.current;
+      if (!el) return;
+      el.style.display = isOpen ? "block" : "none";
+    }, [isOpen]);
     return (
       <section id={windowKey} ref={ref} style={{ zIndex }} className="absolute">
         <Component {...props} />
